94498a8ad092c51cda98a034b13ad55c87133c91
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-08 02:46:24 +0300

    list fixes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,31 +2,21 @@ import Contact from "../Contact/Contact";
 
 import css from "./ContactList.module.css";
 
-export const ContactList = ({ contacts, onDelete }) => {
-  if (!contacts.length) {
-    return (
-      <div className="sub-card">
-        <h2 className={css.listHeader}>Contacts List</h2>
-
-        <ul className={css.contactsList}>
-          <p>No contacts found</p>
-        </ul>
-      </div>
-    );
-  }
-
-  return (
-    <div className="sub-card">
-      <h2 className={css.listHeader}>Contacts List</h2>
-      <ul className={css.contactsList}>
-        {contacts.map((contact) => (
+export const ContactList = ({ contacts, onDelete }) => (
+  <div className="sub-card">
+    <h2 className={css.listHeader}>Contacts List</h2>
+    <ul className={css.contactsList}>
+      {contacts.length ? (
+        contacts.map((contact) => (
           <li key={contact.id}>
             <Contact data={contact} onDelete={onDelete} />
           </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+        ))
+      ) : (
+        <p>No contacts found</p>
+      )}
+    </ul>
+  </div>
+);
 
 export default ContactList;
